240fc1739b9295ad1fa16f2ca508fe20c0fef8a5
wataru <top.men09@gmail.com>
2022-03-21 06:22:57 +0900

    feat: turborepoを導入し、ui用のpackageを追加

---
+++ apps/next-app/pages/about/index.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <h1>This is About Page in Next App!</h1>;
+};
+
+export default About;
---
+++ packages/ui/dist/index.mjs
@@ -0,0 +1,8 @@
+// src/Button/Button.tsx
+import * as React from "react";
+var Button = () => {
+  return /* @__PURE__ */ React.createElement("button", null, "this button is from ui lib");
+};
+export {
+  Button
+};
